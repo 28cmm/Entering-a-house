c2d79164347a8e38105f9466fa5ed06538e7e982
28cmm <joshuafang1998@gmail.com>
2019-01-16 18:14:04 -0800

    done

--- Entering a House/ViewController.m
+++ Entering a House/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UINavigationItem *firstPage;
 
 @end
 
@@ -17,6 +18,21 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+//    UIBarButtonItem *anotherButton = [[UIBarButtonItem alloc] initWithTitle:@"Show" style:UIBarButtonItemStylePlain target:self action:@selector(refreshPropertyList:)];
+//    self.navigationItem.rightBarButtonItem = anotherButton;
+//    // exclude the following in ARC projects...
+//    [anotherButton release];
+    UIBarButtonItem *anotherButton = [[UIBarButtonItem alloc] initWithTitle:@"home" style:UIBarButtonItemStylePlain target:self action:@selector(refreshPropertyList)];
+    self.navigationItem.rightBarButtonItem = anotherButton;
+    
+    
+    // exclude the following in ARC projects...
+    //[anotherButton release];
+}
+
+-(void)refreshPropertyList{
+    [self.navigationController popToRootViewControllerAnimated:NO];
+    
 }
 
 
